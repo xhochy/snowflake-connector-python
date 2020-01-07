413a9d941220cb87168b9b371b5a7fb86432bac1
Shige Takeda <shige.takeda@snowflake.com>
2020-01-06 16:42:40 -0800

    test gitworkflow

---
+++ scripts/run_github_workflow.sh
@@ -0,0 +1,45 @@
+#!/bin/bash -e
+#
+# Run Travis Tests
+#
+
+# shellcheck disable=SC2034
+THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+set -o pipefail
+if [ "$TRAVIS_OS_NAME" == "osx" ]; then
+    TIMEOUT_CMD=("gtimeout" "-s" "SIGUSR1" "3600s")
+else
+    TIMEOUT_CMD=("timeout" "-s" "SIGUSR1" "3600s")
+fi
+TEST_GROUP_CMD=()
+if [[ -n "$SNOWFLAKE_TEST_GROUP" ]]; then
+    TEST_GROUP_CMD=("--test-group-count" "$SNOWFLAKE_TEST_GROUP_TOTAL" "--test-group=$SNOWFLAKE_TEST_GROUP" "--test-group-random-seed=12345")
+fi
+source ./venv/bin/activate
+ret=0
+if [ -n "$SNOWFLAKE_AZURE" ]; then
+  echo "Running Azure tests only..."
+  # shellcheck disable=SC2068
+  ${TIMEOUT_CMD[@]} py.test -vvv --cov=snowflake.connector \
+  --cov-report=xml:python_connector_${TRAVIS_PYTHON_VERSION}_coverage.xml \
+  ${TEST_GROUP_CMD[@]} \
+  -m "putget" test || ret=$?
+elif [ -n "$SNOWFLAKE_GCP" ]; then
+  echo "Running GCP tests only..."
+  # shellcheck disable=SC2068
+  ${TIMEOUT_CMD[@]} py.test -vvv --cov=snowflake.connector \
+  --cov-report=xml:python_connector_${TRAVIS_PYTHON_VERSION}_coverage.xml \
+  ${TEST_GROUP_CMD[@]} \
+  -m "putget" test || ret=$?
+else
+  echo "Running regular tests..."
+  # shellcheck disable=SC2068
+  ${TIMEOUT_CMD[@]} py.test -vvv --cov=snowflake.connector \
+  --cov-report=xml:python_connector_${TRAVIS_PYTHON_VERSION}_coverage.xml \
+  ${TEST_GROUP_CMD[@]} \
+  test || ret=$?
+fi
+
+# TIMEOUT or SUCCESS
+[ $ret != 124 -a $ret != 0 ] && exit 1 || exit 0
